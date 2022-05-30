6565736a9813b933ac7244120774401cf94a6676
Mat <66589714+PxLib@users.noreply.github.com>
2022-05-30 02:41:10 +0200

    Initial commit

---
+++ src/main/java/fun/mcbee/plugin/skyblockaddon/SkyBlockAddon.java
@@ -0,0 +1,17 @@
+package fun.mcbee.plugin.skyblockaddon;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SkyBlockAddon extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
